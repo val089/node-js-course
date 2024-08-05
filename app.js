8ea6ed05a29a5f039ca8461646f3457c6664b2a2
kamil.szerlag <kamil.szerlag>
2024-08-05 11:41:42 +0200

    lesson-6

--- app.js
+++ app.js
@@ -2,20 +2,11 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHandlebars = require('express-handlebars');
 
 const app = express();
 
 //handlebars zamiast pug
-app.engine(
-  'hbs',
-  expressHandlebars({
-    layoutsDir: 'views/layouts/',
-    defaultLayout: 'main-layout',
-    extname: 'hbs'
-  })
-);
-app.set('view engine', 'hbs'); // tak mogę zmieniać rozszerzenie  pliku
+app.set('view engine', 'ejs'); // tak mogę zmieniać rozszerzenie  pliku
 
 // set global configuration for the view engine
 // pug engine is used to render the views
--- routes/shop.js
+++ routes/shop.js
@@ -14,7 +14,8 @@ router.get('/', (req, res, next) => {
 
   res.render('shop', {
     products,
-    pageTtile: 'Shop',
+    pageTitle: 'Shop',
+    path: '/',
     hasProducts: products.length > 0,
     activeShop: true,
     productCSS: true,
