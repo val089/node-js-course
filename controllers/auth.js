ea6d5cb90a0ead8fc0ecd22a73b2ae13a342ce8e
kamil.szerlag <kamil.szerlag>
2025-03-26 13:13:41 +0100

    lesson-18/Understanding-validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,6 +4,7 @@ const nodemailer = require('nodemailer');
 const crypto = require('crypto');
 // const sendgridTransport = require('nodemailer-sendgrid-transport');
 const sendgridMail = require('@sendgrid/mail');
+const { validationResult } = require('express-validator');
 
 const User = require('../models/user');
 const path = require('path');
@@ -16,9 +17,9 @@ const path = require('path');
 //   })
 // );
 
-if (!process.env.SENDGRID_API_KEY) {
-  throw new Error('SENDGRID_API_KEY is not set');
-}
+// if (!process.env.SENDGRID_API_KEY) {
+//   throw new Error('SENDGRID_API_KEY is not set');
+// }
 
 sendgridMail.setApiKey(process.env.SENDGRID_API_KEY);
 
@@ -50,13 +51,25 @@ exports.getSignup = (req, res, next) => {
   res.render('auth/signup', {
     path: '/signup',
     pageTitle: 'Signup',
-    errorMessage: message
+    errorMessage: message,
+    oldInput: { email: '', password: '', confirmPassword: '' },
+    validationErrors: []
   });
 };
 
 exports.postLogin = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
+
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).render('auth/login', {
+      path: '/login',
+      pageTitle: 'Login',
+      errorMessage: errors.array()[0].msg
+    });
+  }
 
   User.findOne({ email })
     .then((user) => {
@@ -92,46 +105,52 @@ exports.postLogin = (req, res, next) => {
 exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
-  const confirmPassword = req.body.confirmPassword;
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).render('auth/signup', {
+      path: '/signup',
+      pageTitle: 'Signup',
+      errorMessage: errors.array()[0].msg,
+      oldInput: { email, password, confirmPassword: req.body.confirmPassword },
+      validationErrors: errors.array()
+    });
+  }
 
   User.findOne({
     email
-  })
-    .then((userDoc) => {
-      if (userDoc) {
-        req.flash('error', 'Email already exists. Please use another email.');
-        return res.redirect('/signup');
-      }
+  }).then((userDoc) => {
+    if (userDoc) {
+      req.flash('error', 'Email already exists. Please use another email.');
+      return res.redirect('/signup');
+    }
 
-      // 12 - number of rounds for hashing, is highly secure
-      return bcrypt
-        .hash(password, 12)
-        .then((hashedPassword) => {
-          const user = new User({
-            email,
-            password: hashedPassword,
-            cart: { items: [] }
-          });
-
-          return user.save();
-        })
-        .then(() => {
-          res.redirect('/login');
-          console.log('Signup succeeded!');
-          return sendgridMail.send({
-            to: email,
-            from: 'kamilszerlag@gmail.com',
-            subject: 'Signup succeeded!',
-            html: '<h1>You successfully signed up!</h1>'
-          });
-        })
-        .catch((err) => {
-          console.log(err);
+    // 12 - number of rounds for hashing, is highly secure
+    return bcrypt
+      .hash(password, 12)
+      .then((hashedPassword) => {
+        const user = new User({
+          email,
+          password: hashedPassword,
+          cart: { items: [] }
         });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+
+        return user.save();
+      })
+      .then(() => {
+        res.redirect('/login');
+        console.log('Signup succeeded!');
+        return sendgridMail.send({
+          to: email,
+          from: 'kamilszerlag@gmail.com',
+          subject: 'Signup succeeded!',
+          html: '<h1>You successfully signed up!</h1>'
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
 };
 
 exports.postLogout = (req, res, next) => {
--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const { check, body } = require('express-validator');
+
+const User = require('../models/user');
 
 const authController = require('../controllers/auth');
 
@@ -8,9 +11,53 @@ router.get('/login', authController.getLogin);
 
 router.get('/signup', authController.getSignup);
 
-router.post('/login', authController.postLogin);
+router.post(
+  '/login',
+  [
+    body('email').isEmail().withMessage('Please enter a valid email address.'),
+    body('password', 'Password has to be valid.')
+      .isLength({ min: 5 })
+      .isAlphanumeric()
+  ],
+  authController.postLogin
+);
 
-router.post('/signup', authController.postSignup);
+router.post(
+  '/signup',
+  [
+    check('email')
+      .isEmail()
+      .withMessage('Please enter a valid email.')
+      // Custom validator
+      .custom((value, { req }) => {
+        // if (value === 'test@test.com') {
+        //   throw new Error('This email address is forbidden.');
+        // }
+        // return true;
+
+        return User.findOne({ email: value }).then((userDoc) => {
+          if (userDoc) {
+            return Promise.reject(
+              'E-Mail exists already, please pick a different one.'
+            );
+          }
+        });
+      }),
+    body(
+      'password',
+      'Please enter a password with only numbers and text at least 5 characters.'
+    )
+      .isLength({ min: 5 })
+      .isAlphanumeric(),
+    body('confirmPassword').custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error('Passwords have to match!');
+      }
+      return true;
+    })
+  ],
+  authController.postSignup
+);
 
 router.post('/logout', authController.postLogout);
 
